375aac9f922217e3b281fd534f9bea74ce5812fd
daolotos <117892786+daolotos@users.noreply.github.com>
2023-02-19 13:39:06 +0300

    Дописала 1 задачу

--- Task1.py
+++ Task1.py
@@ -1,13 +1,14 @@
 def SumRecursive(x1, x2):
-    if x1 > 0:
-        return SumRecursive(x1 - 1, x2 + 1)
-    elif x1 < 0:
-        return SumRecursive(x1 + 1, x2 - 1)
-    return x2
+    if x2 > 1:
+        return x1 * SumRecursive(x1, x2 - 1)
+    elif x2 == 0:
+        return 1
+    else:
+        return x1
 
 
 a = int(input("A = "))
 b = int(input("B = "))
 
-print(f"sum = {SumRecursive(a, b)}")
+print(f"sum = {pow(a, b)}")
 
